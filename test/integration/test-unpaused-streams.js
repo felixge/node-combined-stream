938c8d5ad05da5dcd14d9faa9ac433265fc5a019
Felix Geisendörfer <felix@debuggable.com>
2011-05-24 08:57:33 +0200

    Implemented pauseStreams and turned it on by default

--- test/integration/test-unpaused-streams.js
+++ test/integration/test-unpaused-streams.js
@@ -8,7 +8,7 @@ var FILE2 = common.dir.fixture + '/file2.txt';
 var EXPECTED = fs.readFileSync(FILE1) + fs.readFileSync(FILE2);
 
 (function testDelayedStreams() {
-  var combinedStream = CombinedStream.create();
+  var combinedStream = CombinedStream.create({pauseStreams: false});
   combinedStream.append(fs.createReadStream(FILE1));
   combinedStream.append(fs.createReadStream(FILE2));
 
@@ -16,7 +16,7 @@ var EXPECTED = fs.readFileSync(FILE1) + fs.readFileSync(FILE2);
   var stream2 = combinedStream._streams[1];
 
   stream1.on('end', function() {
-    assert.equal(stream2.dataSize, 0);
+    assert.ok(stream2.dataSize > 0);
   });
 
   var tmpFile = common.dir.tmp + '/combined.txt';
