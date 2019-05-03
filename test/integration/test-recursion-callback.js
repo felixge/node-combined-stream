3a537f21fe5efcf04779b4054bd0a5b6a8aea0e8
Alba Mendez <me@alba.sh>
2019-05-03 14:02:07 +0200

    Add two tests for recursion

---
+++ test/integration/test-recursion-callback.js
@@ -0,0 +1,13 @@
+var common = require('../common');
+var assert = common.assert;
+var CombinedStream = common.CombinedStream;
+
+var s = CombinedStream.create();
+
+for (var i = 0; i < 1e4; i++)
+    s.append(function (next) {
+        next('test')
+    });
+
+// Shouldn't throw "RangeError: Maximum call stack size exceeded"
+s.resume();
